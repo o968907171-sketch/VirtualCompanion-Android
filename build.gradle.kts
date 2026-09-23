f9404be29a2132c4d8d2dedabb75b620c141a4f1
o968907171-sketch <o968907171@gmail.com>
2026-09-23 11:13:44 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,30 @@
 plugins {
-    id("com.android.application") version "8.9.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.virtualcompanion"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.virtualcompanion"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 9
+        versionName = "0.7.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
 }
